22fff35ab89d2e9cd9450cbad909a380aba375e3
larisamgs <lary.mogos@gmail.com>
2022-06-10 16:22:06 +0300

    added icon buttons to change gradient direction

--- script.js
+++ script.js
@@ -3,18 +3,65 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 let rand = document.getElementById("random");
+let iconButtons = document.getElementsByClassName("btn-icons");
+let iconDirection;
+
+for (let iconButton of iconButtons) {
+  iconButton.addEventListener("click", changeGradientDirection);
+}
+
+function changeGradientDirection(event) {
+  let buttonOrIconElement = event.target;
+  if (buttonOrIconElement.tagName === "BUTTON") {
+    buttonOrIconElement = buttonOrIconElement.children[0];
+  }
+  const iconClassThatContainsDirectionInformation =
+    buttonOrIconElement.classList[1];
+
+  if (iconClassThatContainsDirectionInformation === "fa-square-caret-down") {
+    iconDirection = "to bottom";
+  } else if (
+    iconClassThatContainsDirectionInformation === "fa-square-caret-up"
+  ) {
+    iconDirection = "to top";
+  } else if (
+    iconClassThatContainsDirectionInformation === "fa-square-caret-left"
+  ) {
+    iconDirection = "to left";
+  } else if (
+    iconClassThatContainsDirectionInformation === "fa-square-caret-right"
+  ) {
+    iconDirection = "to right";
+  } else {
+    console.log("default");
+  }
+
+  body.style.background =
+    "linear-gradient(" +
+    iconDirection +
+    ", " +
+    color1.value +
+    ", " +
+    color2.value +
+    ")";
+}
 
 body.style.background =
   "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-
 newCssBg.textContent = body.style.background + ";";
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
 function setGradient() {
+  console.log(iconDirection);
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    "linear-gradient(, " +
+    iconDirection +
+    color1.value +
+    ", " +
+    color2.value +
+    ")";
   newCssBg.textContent = body.style.background + ";";
 }
 
@@ -24,7 +71,12 @@ function generateRandomcolors() {
   let randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   let randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
   body.style.background =
-    "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
+    "linear-gradient(, " +
+    iconDirection +
+    randomColor1 +
+    ", " +
+    randomColor2 +
+    ")";
   newCssBg.textContent = body.style.background + ";";
   color1.value = randomColor1;
   color2.value = randomColor2;
